a5b08e0669a1329327dda78b598bae9ee53f524e
Ilia Petrovskii <ipetrovskii1301@gmail.com>
2022-10-25 14:34:13 +0300

    Наследование

--- src/Inheritance.ts
+++ src/Inheritance.ts
@@ -16,6 +16,20 @@ export class Person {
     readonly _name: string;
     readonly _secondName: string;
     readonly _age: number;
+
+    constructor(data: IPersonData) {
+        this._name = data.name;
+        this._secondName = data.secondName;
+        this._age = data.age;
+    }
+
+    getData(): IPersonData {
+        return {
+            name: this._name,
+            secondName: this._secondName,
+            age: this._age,
+        };
+    }
 }
 
 /*
@@ -24,4 +38,18 @@ export class Person {
  */
 export class Student extends Person {
     readonly _phone: string;
+
+    constructor(data: IStudentData) {
+        super(data);
+        this._phone = data.phone;
+    }
+
+    getData(): IStudentData {
+        return {
+            name: this._name,
+            secondName: this._secondName,
+            age: this._age,
+            phone: this._phone,
+        };
+    }
 }
